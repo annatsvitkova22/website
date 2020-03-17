80b2d5f22b57710e018b9739603c0e833d7c9297
Vladyslav Romanenko <vladyslavromanenko@gmail.com>
2020-03-17 22:38:17 +0200

    Add base header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Navigation from "./Navigation";
+import Link from 'next/link';
+import '../styles/components/header.scss';
 
 const Header = () => {
   return (
     <header>
+      <Link href={'/'}>
+        <a>Logo</a>
+      </Link>
       <Navigation />
     </header>
   );
