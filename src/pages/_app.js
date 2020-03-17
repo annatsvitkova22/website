39a3e48f4db4e69f6f4be500201d35b2d4691bc0
Vladyslav Romanenko <vladyslavromanenko@gmail.com>
2020-03-17 20:52:44 +0200

    Start writing custom _app

---
+++ src/pages/_app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import App from 'next/app';
+
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+}
+
+// You can comment this method below if you don't want to have every single page rendered on server
+// (but looks like we need to have this for Zmist)
+// This method disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+MyApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+
+  return { ...appProps };
+}
+
+export default MyApp;
