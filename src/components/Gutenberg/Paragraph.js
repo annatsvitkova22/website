9717fb37e8504a50cf43ca94ce502bd66de1bbfb
Vladyslav Romanenko <vladyslavromanenko@gmail.com>
2020-03-30 20:00:01 +0300

    Start defining Gutenberg blocks

---
+++ src/components/Gutenberg/Paragraph.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Paragraph = ({ block }) => {
+  // TODO: implement all other attributs
+  return <p>{block.attributes.content}</p>;
+};
+
+export default Paragraph;
