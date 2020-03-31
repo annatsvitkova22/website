83eae78da224da965f66945a1962e4256bb53dec
andrii-ohiienko <andrii.ohiienko@outright.digital>
2020-03-31 18:35:06 +0300

    add blogs routing

---
+++ src/pages/blogs/[slug].js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Head from 'next/head';
+import gql from 'graphql-tag';
+
+import apolloClient from '~/lib/ApolloClient';
+
+const BLOG = gql`
+  query Blog($slug: String!) {
+    blogBy(slug: $slug) {
+      content
+      title
+    }
+  }
+`;
+
+const Blog = (props) => {
+  const { blog } = props;
+  return (
+    <div className="single-news">
+      <Head>
+        <title>{blog.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">{blog.title}</h1>
+
+        <div className="description">{blog.content}</div>
+      </main>
+    </div>
+  );
+};
+
+Blog.getInitialProps = async ({ query: { slug } }) => {
+  const { data } = await apolloClient.query({
+    query: BLOG,
+    variables: { slug },
+  });
+
+  return {
+    blog: data.blogBy,
+  };
+};
+
+export default Blog;
