397ba4c5a295996eea9348c87a9e115afb035644
Deekline <46416799+Deekline@users.noreply.github.com>
2020-04-01 12:39:23 +0300

    Add Audio gutenberg component

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import Paragraph from './Gutenberg/Paragraph';
 import Image from './Gutenberg/Image';
+import Audio from '~/components/Gutenberg/Audio';
 
 const Content = ({ content }) => {
   // TODO: add & test all content types listed in this log
@@ -17,6 +18,9 @@ const Content = ({ content }) => {
         }
         if (block.__typename === 'CoreImageBlock') {
           return <Image block={block} key={`${block.__typename}-${index}`} />;
+        }
+        if (block.__typename === 'CoreAudioBlock') {
+          return <Audio block={block} key={`${block.__typename}-${index}`} />;
         }
         return null;
       })}
---
+++ src/components/Gutenberg/Audio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Audio = ({ block }) => {
+  if (block.attributes) {
+    return (
+      <figure>
+        <figcaption>{block.attributes.caption}</figcaption>
+        <audio controls src={block.attributes.src} />
+      </figure>
+    );
+  }
+  return <audio controls src="#" />;
+};
+
+export default Audio;
+
+Audio.propTypes = {
+  block: PropTypes.any,
+};
