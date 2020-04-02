5a7fa0e79b5b066156a009b5c78ca9c4c0385a89
Vladyslav Romanenko <vladyslavromanenko@gmail.com>
2020-04-02 18:02:51 +0300

    Fix linting errors

--- src/hooks/useFilterHook.js
+++ src/hooks/useFilterHook.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const useFilterHook = (type, eventsData, date) => {
   const [data, setData] = useState(eventsData);
