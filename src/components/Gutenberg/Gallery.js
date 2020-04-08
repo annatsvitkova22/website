bcc342364084f7724ed5b0a854a2b64463e90ba4
Deekline <46416799+Deekline@users.noreply.github.com>
2020-04-08 18:13:08 +0300

    fix typography

--- src/components/Gutenberg/Gallery.js
+++ src/components/Gutenberg/Gallery.js
@@ -5,15 +5,21 @@ const Gallery = ({ block }) => {
   const array = block.attributes.images;
   const reg = /[\[\]\{\}\\]/gm;
   const regTwo = /\,/gm;
+  const anotherreg = /(^.*:")/gm;
   const newArr = array.replace(reg, '').replace(regTwo, ' ').split(' ');
-  const chunk = newArr.length / 6;
+  const chunk = newArr.length / 7;
   const splitArr = (arr, chunks) =>
     arr.reduce(
       // eslint-disable-next-line no-return-assign
       (acc, n, i) => ((acc[i % chunks] = acc[i % chunks] || []).push(n), acc),
       []
     );
-  splitArr(newArr, chunk);
+  const arr = splitArr(newArr, chunk);
+  const newData = arr.map((item) => {
+    return item.map((element) =>
+      element.replace(anotherreg, '').replace('"', '')
+    );
+  });
 
   return <div dangerouslySetInnerHTML={{ __html: block.saveContent }} />;
 };
