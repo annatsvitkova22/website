b6cb45e20a1026b682b8b02986413a56967eb74f
Deekline <46416799+Deekline@users.noreply.github.com>
2020-04-14 14:55:09 +0300

    Add Actionbar skeleton component

--- src/components/Loaders/PostCardLoader.js
+++ src/components/Loaders/PostCardLoader.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
 
-const PostCardLoader = ({ width }) => {
-  if (width === 'small') {
+const PostCardLoader = ({ type }) => {
+  if (type === 'small') {
     return (
       <ContentLoader viewBox={'0 0 425 325'}>
         <rect x={'0'} y={'0'} rx={'0'} rx={'0'} width={'425'} height={`250`} />
@@ -10,14 +10,14 @@ const PostCardLoader = ({ width }) => {
       </ContentLoader>
     );
   }
-  if (width === 'big') {
+  if (type === 'big') {
     return (
       <ContentLoader viewBox={'0 0 648 325'}>
         <rect x={'0'} y={'0'} rx={'0'} rx={'0'} width={'648'} height={`430`} />
       </ContentLoader>
     );
   }
-  if (width === 'wide') {
+  if (type === 'wide') {
     return (
       <ContentLoader viewBox={'0 0 1320 410'}>
         <rect x={'0'} y={'0'} rx={'0'} rx={'0'} width={'655'} height={`410`} />
