6b7d2a513e9ab1edff6cebe2c0dc497a55062b5f
Deekline <46416799+Deekline@users.noreply.github.com>
2020-04-14 14:47:50 +0300

    Add Postcard skeleton component for diferent width

---
+++ src/components/Loaders/PostCardLoader.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const PostCardLoader = ({ width }) => {
+  if (width === 'small') {
+    return (
+      <ContentLoader viewBox={'0 0 425 325'}>
+        <rect x={'0'} y={'0'} rx={'0'} rx={'0'} width={'425'} height={`250`} />
+        <rect x={'0'} y={'255'} rx={'0'} rx={'0'} width={'425'} height={`75`} />
+      </ContentLoader>
+    );
+  }
+  if (width === 'big') {
+    return (
+      <ContentLoader viewBox={'0 0 648 325'}>
+        <rect x={'0'} y={'0'} rx={'0'} rx={'0'} width={'648'} height={`430`} />
+      </ContentLoader>
+    );
+  }
+  if (width === 'wide') {
+    return (
+      <ContentLoader viewBox={'0 0 1320 410'}>
+        <rect x={'0'} y={'0'} rx={'0'} rx={'0'} width={'655'} height={`410`} />
+        <rect
+          x={'660'}
+          y={'0'}
+          rx={'0'}
+          rx={'0'}
+          width={'655'}
+          height={`410`}
+        />
+      </ContentLoader>
+    );
+  }
+  return null;
+};
+
+export default PostCardLoader;
