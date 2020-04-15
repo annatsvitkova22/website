1d8e28f7806e079e6891ff96a09509d6baf64ffc
Slava Developer <ad@ad>
2020-04-15 09:15:34 +0300

    fix crowsfundings/index.js conflicts

--- src/pages/crowdfundings/index.js
+++ src/pages/crowdfundings/index.js
@@ -4,7 +4,7 @@ import gql from 'graphql-tag';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import NumberFormat from 'react-number-format';
-//import dateFormat from 'dateformat';
+import dateFormat from 'dateformat';
 
 import apolloClient from '~/lib/ApolloClient';
 import '../../styles/pages/crowdfundings.scss';
