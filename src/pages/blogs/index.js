97c7e807d26f106f14638b46d6fedfef180c72e2
Deekline <46416799+Deekline@users.noreply.github.com>
2020-04-17 13:45:38 +0300

    Add fetching data, dynamic scroll, sceleton loaders for blogs and for first loading

--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -1,25 +1,100 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import gql from 'graphql-tag';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import { Waypoint } from 'react-waypoint';
 
 import apolloClient from '~/lib/ApolloClient';
+import NewsLoader from '~/components/Loaders/NewsLoader';
+import BlogsLoader from '~/components/Loaders/BlogsLoader';
 
 const BLOGS_ARCHIVE = gql`
-  query BlogsArchive {
-    blogs {
+  query BlogsArchive($cursor: String) {
+    blogs(first: 5, before: $cursor) {
       nodes {
         excerpt
         title
         slug
+        id
+      }
+      pageInfo {
+        endCursor
+        total
       }
     }
   }
 `;
 
 const BlogsArchive = (props) => {
-  const { blogs } = props;
+  const [state, setState] = useState({
+    blogs: props,
+    endCursor: props.pageInfo ? props.pageInfo.endCursor : null,
+    isLoading: false,
+  });
+
+  useEffect(() => {
+    if (!state.isLoading) {
+      setState({
+        ...state,
+        isLoading: true,
+      });
+    }
+    async function loadData() {
+      const { data } = await apolloClient.query({
+        query: BLOGS_ARCHIVE,
+        variables: {
+          cursor: null,
+        },
+      });
+      setState({
+        blogs: data.blogs,
+        endCursor: data.blogs.pageInfo.endCursor,
+        isLoading: false,
+      });
+    }
+
+    if (!state.blogs.nodes) {
+      loadData();
+    }
+  }, []);
+
+  const fetchingContent = async () => {
+    if (!state.isLoading) {
+      setState({
+        ...state,
+        isLoading: true,
+      });
+    }
+    const blogsData = await apolloClient.query({
+      query: BLOGS_ARCHIVE,
+      variables: {
+        cursor: state.endCursor,
+      },
+    });
+
+    setState({
+      isLoading: false,
+      endCursor: blogsData.data.blogs.pageInfo
+        ? blogsData.data.blogs.pageInfo.endCursor
+        : false,
+      blogs: {
+        pageInfo,
+        nodes: [...state.blogs.nodes, ...blogsData.data.blogs.nodes],
+      },
+    });
+  };
+
+  if (!state.blogs.nodes)
+    return (
+      <div style={{ margin: '0 auto' }}>
+        <BlogsLoader />
+        <BlogsLoader />
+      </div>
+    );
+
+  const { nodes, pageInfo } = state.blogs;
+
   return (
     <div className="news-page">
       <Head>
@@ -29,16 +104,22 @@ const BlogsArchive = (props) => {
       </Head>
 
       <main>
-        {blogs.map((blog, i) => (
-          <article key={i}>
-            <Link href="/blogs/[slug]" as={`/blogs/${blog.slug}`}>
-              <a>
-                <h3>{blog.title}</h3>
-              </a>
-            </Link>
-            <div>{blog.excerpt}</div>
-          </article>
-        ))}
+        <div>
+          {nodes.map((blog, i) => (
+            <article key={i} style={{ height: '300px' }}>
+              <Link href="/blogs/[slug]" as={`/blogs/${blog.slug}`}>
+                <a>
+                  <h3>{blog.title}</h3>
+                </a>
+              </Link>
+              <div>{blog.excerpt}</div>
+              {i === nodes.length - 1 && i < pageInfo.total - 1 && (
+                <Waypoint onEnter={fetchingContent} />
+              )}
+            </article>
+          ))}
+        </div>
+        {state.isLoading && <BlogsLoader />}
       </main>
     </div>
   );
@@ -55,13 +136,19 @@ BlogsArchive.propTypes = {
 };
 
 BlogsArchive.getInitialProps = async () => {
+  if (process.browser) {
+    return {};
+  }
   const { data } = await apolloClient.query({
     query: BLOGS_ARCHIVE,
+    variables: {
+      cursor: null,
+    },
   });
 
-  return {
-    blogs: data.blogs.nodes,
-  };
+  const { blogs } = data;
+
+  return blogs;
 };
 
 export default BlogsArchive;
