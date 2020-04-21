b57279ce4b5136073e46ed0ef6f472896e81da1d
Deekline <46416799+Deekline@users.noreply.github.com>
2020-04-21 13:40:30 +0300

    Add header skeleton

--- src/components/Loaders/PostHeaderLoader.js
+++ src/components/Loaders/PostHeaderLoader.js
@@ -4,18 +4,22 @@ import ContentLoader from 'react-content-loader';
 const PostHeaderLoader = () => {
   return (
     <div className={'container'}>
-      <ContentLoader viewBox={'0 0 380 932'}>
-        <rect x={'0'} y={'0'} rx={'0'} rx={'0'} width={'120'} height={'15'} />
-        <rect x={'0'} y={'20'} rx={'0'} rx={'0'} width={'875'} height={'70'} />
-        <rect x={'0'} y={'95'} rx={'0'} rx={'0'} width={'875'} height={'70'} />
-        <rect x={'0'} y={'170'} rx={'0'} rx={'0'} width={'875'} height={'15'} />
+      <ContentLoader viewBox={'0 0 1044 700'}>
+        <rect x={'0'} y={'0'} rx={'0'} rx={'0'} width={'60'} height={'15'} />
+        <rect x={'0'} y={'20'} rx={'0'} rx={'0'} width={'875'} height={'15'} />
+        <rect x={'0'} y={'40'} rx={'0'} rx={'0'} width={'875'} height={'15'} />
+        <rect x={'0'} y={'60'} rx={'0'} rx={'0'} width={'875'} height={'15'} />
+        <rect x={'0'} y={'100'} rx={'0'} rx={'0'} width={'875'} height={'10'} />
+        <rect x={'0'} y={'115'} rx={'0'} rx={'0'} width={'875'} height={'10'} />
+        <rect x={'0'} y={'130'} rx={'0'} rx={'0'} width={'875'} height={'10'} />
+        <rect x={'0'} y={'145'} rx={'0'} rx={'0'} width={'875'} height={'20'} />
         <rect
           x={'0'}
           y={'190'}
           rx={'0'}
           rx={'0'}
-          width={'875'}
-          height={'300'}
+          width={'1044'}
+          height={'535'}
         />
       </ContentLoader>
     </div>
