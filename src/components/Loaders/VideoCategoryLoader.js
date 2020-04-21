314d2d5acc8fa7a14713cf696c33c8f2a05a2cd4
Deekline <46416799+Deekline@users.noreply.github.com>
2020-04-21 12:33:57 +0300

    Implement loaders for video category page

---
+++ src/components/Loaders/VideoCategoryLoader.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const VideoCategoryLoader = () => {
+  return (
+    <div className="container">
+      <ContentLoader
+        viewBox={'0 0 1320 500'}
+        backgroundColor={'#333'}
+        foregroundColor={'#999'}
+      >
+        <rect x={'0'} y={'0'} rx={'0'} rx={'0'} width={'312'} height={`190`} />
+        <rect x={'0'} y={'205'} rx={'0'} rx={'0'} width={'30'} height={`17`} />
+        <rect x={'0'} y={'225'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'0'} y={'245'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'0'} y={'265'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'0'} y={'285'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+
+        <rect x={'336'} y={'0'} rx={'0'} rx={'0'} width={'312'} height={`190`} />
+        <rect x={'336'} y={'205'} rx={'0'} rx={'0'} width={'30'} height={`17`} />
+        <rect x={'336'} y={'225'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'336'} y={'245'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'336'} y={'265'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'336'} y={'285'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+
+        <rect x={'672'} y={'0'} rx={'0'} rx={'0'} width={'312'} height={`190`} />
+        <rect x={'672'} y={'205'} rx={'0'} rx={'0'} width={'30'} height={`17`} />
+        <rect x={'672'} y={'225'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'672'} y={'245'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'672'} y={'265'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'672'} y={'285'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+
+        <rect x={'1008'} y={'0'} rx={'0'} rx={'0'} width={'312'} height={`190`} />
+        <rect x={'1008'} y={'205'} rx={'0'} rx={'0'} width={'30'} height={`17`} />
+        <rect x={'1008'} y={'225'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'1008'} y={'245'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'1008'} y={'265'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+        <rect x={'1008'} y={'285'} rx={'0'} rx={'0'} width={'312'} height={`15`} />
+      </ContentLoader>
+    </div>
+  );
+};
+
+export default VideoCategoryLoader;
