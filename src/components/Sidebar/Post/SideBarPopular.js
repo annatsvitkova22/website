03b6081b397ab926555a3d014bfb4122595611cb
Deekline <46416799+Deekline@users.noreply.github.com>
2020-04-24 13:53:58 +0300

    Move all news sidebars to one component and to one directory

--- src/components/Sidebar/Post/SideBarPopular.js
+++ src/components/Sidebar/Post/SideBarPopular.js
@@ -4,12 +4,13 @@ import { Waypoint } from 'react-waypoint';
 
 import SidebarLoader from '~/components/Loaders/SidebarLoader';
 
-const SideBarPopular = ({ news, fetchingContent, isLoading }) => {
+const SideBarPopular = ({ news }) => {
   if (!news) return <SidebarLoader />;
+
   return (
     <ul className={'sidebar-popular'}>
       <li className={'sidebar-popular__title'}>популярне</li>
-      {news.nodes.map((item, i) => {
+      {news.nodes.slice(5).map((item, i) => {
         return (
           <li key={i} className={'sidebar-popular__item'}>
             <a href={item.link}>
@@ -24,7 +25,6 @@ const SideBarPopular = ({ news, fetchingContent, isLoading }) => {
           </li>
         );
       })}
-      {isLoading && <SidebarLoader />}
     </ul>
   );
 };
