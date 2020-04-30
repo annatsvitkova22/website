f9fa23580ad6fdec17e9339a449597bab9a8b1c1
andrii-ohiienko <andrii.ohiienko@outright.digital>
2020-04-30 13:52:04 +0300

    Add publications to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import {
   options,
 } from '~/components/PhotoSwipeGallery/videoGalleryUtils';
 import addVideoDurations from '~/util/addVideoDurations';
+import Article from '~/components/Article';
 
 // TODO: restore, create custom GraphQL resolver
 // homepage {
@@ -43,11 +44,78 @@ const HOME_PAGE = gql`
         }
       }
     }
+    publications(first: 6) {
+      nodes {
+        excerpt
+        title
+        slug
+        featuredImage {
+          mediaItemUrl
+        }
+        categories {
+          nodes {
+            id
+            name
+            slug
+          }
+        }
+        author {
+          name
+          nicename
+          nickname
+          slug
+          userId
+          username
+        }
+        zmPublicationsACF {
+          size
+          style
+        }
+      }
+      pageInfo {
+        endCursor
+        total
+      }
+    }
+    categories {
+      nodes {
+        name
+        slug
+        zmCategoryACF {
+          order
+          showOnPublications
+          size
+        }
+        publications {
+          nodes {
+            slug
+            title
+            author {
+              slug
+              name
+            }
+            featuredImage {
+              mediaItemUrl
+            }
+          }
+        }
+      }
+    }
   }
 `;
 
 const Home = (props) => {
-  const { page } = props;
+  const { page, videos, publications, categories } = props;
+
+  const filteredCategories = categories.nodes.filter(
+    ({ zmCategoryACF: { showOnPublications } }) => showOnPublications === true
+  );
+
+  const sortedCategories = filteredCategories.sort(
+    (categoryA, categoryB) =>
+      categoryA.zmCategoryACF.order - categoryB.zmCategoryACF.order
+  );
+
   return (
     <div className="home-page">
       <Head>
@@ -67,10 +135,7 @@ const Home = (props) => {
               </h6>
             </div>
             <div className="col-6 text-right tx-green">
-              <Link
-                href={`/videos`}
-                // as={`/videos/category/${category.slug}`}
-              >
+              <Link href={`/videos`}>
                 <a className="video-category__watch-all tx-family-titles font-weight-semibold">
                   Дивись Усі
                 </a>
@@ -80,11 +145,109 @@ const Home = (props) => {
           <div className="row">
             <PhotoSwipeGallery
               className="col-12 video-cat-gall"
-              items={prepareGalleryItems(props.videos)}
+              items={prepareGalleryItems(videos)}
               options={options()}
               thumbnailContent={getThumbnailVideo}
               playClass="tx-white"
             />
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h6 className="publ-page__title text-uppercase">публікації</h6>
+            </div>
+          </div>
+          <div className="last-publs">
+            <div className="row">
+              {publications.nodes.map((post) => (
+                <Article type="publications" post={post} key={post.id} />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            {sortedCategories
+              .slice(0, 4)
+              .map(
+                ({
+                  publications: { nodes },
+                  name,
+                  slug,
+                  zmCategoryACF: { order, size },
+                }) => {
+                  let colSize = '';
+                  switch (size) {
+                    case 'medium':
+                      colSize = 'col-xl-3';
+                      break;
+                    case 'big':
+                      colSize = 'col-xl-4';
+                      break;
+                    case 'small':
+                      colSize = 'col-xl-2';
+                      break;
+
+                    default:
+                      break;
+                  }
+
+                  return (
+                    <div className={`publ-cat__col--${size} ${colSize}`}>
+                      <h6 className="publ-page__title text-uppercase">
+                        <Link href={`/search?category=${slug}`}>
+                          <a>{name}</a>
+                        </Link>
+                      </h6>
+                      <div className="publ-cats__container">
+                        {size === 'big' && nodes.length && (
+                          <>
+                            <div className="row main-cat__row main-cat__row--primary">
+                              {nodes.slice(0, 1).map((post, i) => (
+                                <Article
+                                  isFirst={true}
+                                  size={size}
+                                  type="publications-cats"
+                                  post={post}
+                                  key={post.id}
+                                />
+                              ))}
+                            </div>
+                            <div className="row main-cat__row main-cat__row--sub">
+                              {nodes.slice(1, nodes.length).map((post, i) => (
+                                <Article
+                                  isFirst={false}
+                                  index={i}
+                                  size={size}
+                                  type="publications-cats"
+                                  post={post}
+                                  key={post.id}
+                                />
+                              ))}
+                            </div>
+                          </>
+                        )}
+                        {!(size === 'big') && nodes.length !== 1 && (
+                          <div className="row">
+                            {nodes.map((post, i) => (
+                              <Article
+                                index={i}
+                                size={size}
+                                type="publications-cats"
+                                post={post}
+                                key={post.id}
+                              />
+                            ))}
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  );
+                }
+              )}
           </div>
         </div>
       </main>
@@ -100,14 +263,18 @@ Home.propTypes = {
 };
 
 Home.getInitialProps = async () => {
-  const result = await client.query({
+  const { data } = await client.query({
     query: HOME_PAGE,
   });
 
+  const { pages, videos, publications, categories } = data;
+
   return {
-    page: result.data.pages.nodes[0],
+    page: pages.nodes[0],
     // TODO: Put bellow function on frontend
-    videos: await addVideoDurations(result.data.videos.nodes),
+    videos: await addVideoDurations(videos.nodes),
+    publications,
+    categories,
   };
 };
 
