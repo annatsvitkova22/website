63b1747830a1ee0396cf3637376de9bbb4671698
Vlad Romanenko <vlad@outright.digital>
2020-05-01 16:46:20 +0300

    Add general page loader

--- src/pages/[uri].js
+++ src/pages/[uri].js
@@ -1,32 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 
 import apolloClient from '~/lib/ApolloClient';
+import gutenbergBlocksQuery from '~/lib/GraphQL/gutenbergBlocksQuery';
+import Content from '~/components/Content';
 
 const PAGE = gql`
   query Page($uri: String!) {
     pageBy(uri: $uri) {
       title
-      content
+      ${gutenbergBlocksQuery}
     }
   }
 `;
 
 const Page = (props) => {
-  const { page } = props;
-  return (
-    <div className="page">
-      <Head>
-        <title>{page.title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const [state, setState] = useState({ page: props.page });
 
-      <main>
-        <h1 className="title">{page.title}</h1>
-        <div className="description">{page.content}</div>
-      </main>
+  const { page } = state;
+
+  useEffect(() => {
+    const loadPage = async () => {
+      const { data } = await apolloClient.query({
+        query: PAGE,
+        variables: { uri: props.query.uri },
+      });
+
+      setState({ page: data.pageBy });
+    };
+
+    if (!page) {
+      loadPage();
+    }
+  }, []);
+
+  if (!page) {
+    return (
+      <div className="page">
+        <div className="container">
+          <div className="row">
+            <main className="col-12">
+              loading
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="page">
+        <Head>
+          <title>{page.title}</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div className="container">
+          <div className="row">
+            <main className="col-12">
+              <h1 className="title">{page.title}</h1>
+              <Content content={page.blocks} />
+            </main>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -35,13 +75,18 @@ Page.propTypes = {
   page: PropTypes.any,
 };
 
-Page.getInitialProps = async ({ query: { uri } }) => {
+Page.getInitialProps = async ({ query }) => {
+  if (process.browser) {
+    return { query };
+  }
+
   const { data } = await apolloClient.query({
     query: PAGE,
-    variables: { uri },
+    variables: { uri: query.uri },
   });
 
   return {
+    query,
     page: data.pageBy,
   };
 };
