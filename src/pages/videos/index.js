39d67d275b628f3dc12c4df6eca3dfc1a71841a8
Vlad Romanenko <vlad@outright.digital>
2020-05-01 14:59:14 +0300

    Rewrite video page. Add loader

--- src/pages/videos/index.js
+++ src/pages/videos/index.js
@@ -1,7 +1,8 @@
-import React, { Component, createRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Head from 'next/head';
 import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
+import { Waypoint } from 'react-waypoint';
 
 import VideosList from '~/components/VideosList';
 import VideoCategories from '~/components/VideoCategories';
@@ -13,6 +14,7 @@ import {
 } from '~/util';
 import Play from '~/static/images/play';
 import VideoLoader from '~/components/Loaders/VideoLoader';
+import VideosPageLoader from '~/components/Loaders/VideosPageLoader';
 
 const VIDEOS_ARCHIVE = gql`
   query VideosArchive {
@@ -83,52 +85,97 @@ const VIDEOS_CATEGORIES = gql`
   }
 `;
 
-class VideosArchive extends Component {
-  constructor(props) {
-    super(props);
-    const { title, zmVideoACF } = this.props.videos[0];
-    const { videoUrl, videoCover, duration } = zmVideoACF;
-    this.state = {
-      selectedVideo: {
-        url: formatYouTubeUrl(videoUrl),
-        imageUrl: videoCover.mediaItemUrl,
-        duration,
-        title,
-      },
-      selectedIndex: 0,
-      isPlaying: false,
-      categories: this.props.categories,
-      isLoading: false,
-      endCursor: this.props.endCursor,
-      hasNextPage: this.props.hasNextPage,
+// TODO: refactor whole component, combine 2 states into 1
+const VideosArchive = (props) => {
+  const categoriesRef = useRef(null);
+  const [content, setContent] = useState(props);
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    const loadContent = async () => {
+      const { data } = await apolloClient.query({
+        query: VIDEOS_ARCHIVE,
+      });
+
+      const videoDurations = await getDurations(data.videos.nodes);
+
+      const videos = data.videos.nodes.map((node) => {
+        const { zmVideoACF } = node;
+        const videoId = zmVideoACF.videoUrl.split('?v=')[1];
+
+        return {
+          ...node,
+          zmVideoACF: {
+            ...zmVideoACF,
+            duration: videoDurations[videoId],
+          },
+        };
+      });
+
+      const categories = addCategoryVideosDurations(
+        data.categories.nodes,
+        videoDurations
+      );
+
+      setContent({
+        videoDurations,
+        endCursor: data.categories.pageInfo.endCursor,
+        hasNextPage: data.categories.pageInfo.hasNextPage,
+        videos,
+        categories,
+      });
+
+      const { title, zmVideoACF } = videos[0];
+      const { videoUrl, videoCover, duration } = zmVideoACF;
+
+      setState({
+        ...state,
+        selectedVideo: {
+          url: formatYouTubeUrl(videoUrl),
+          imageUrl: videoCover.mediaItemUrl,
+          duration,
+          title,
+        },
+        selectedIndex: 0,
+        isPlaying: false,
+        categories: categories,
+        isLoading: false,
+        endCursor: data.categories.pageInfo.endCursor,
+        hasNextPage: data.categories.pageInfo.hasNextPage,
+      });
     };
 
-    this.categoriesRef = createRef();
-  }
+    if (!content.videos) {
+      loadContent();
+    } else {
+      const { title, zmVideoACF } = content.videos[0];
+      const { videoUrl, videoCover, duration } = zmVideoACF;
 
-  componentDidMount() {
-    if (this.state.hasNextPage) {
-      window.addEventListener('scroll', this.onScroll);
+      setState({
+        selectedVideo: {
+          url: formatYouTubeUrl(videoUrl),
+          imageUrl: videoCover.mediaItemUrl,
+          duration,
+          title,
+        },
+        selectedIndex: 0,
+        isPlaying: false,
+        categories: content.categories,
+        isLoading: false,
+        endCursor: content.endCursor,
+        hasNextPage: content.hasNextPage,
+      });
     }
-  }
+  }, []);
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.onScroll);
-  }
-
-  onScroll = () => {
-    const { offsetTop, offsetHeight } = this.categoriesRef.current;
-    if (offsetTop + offsetHeight >= window.scrollY) {
-      this.onLoadMore();
-    }
-  };
-
-  onLoadMore = async () => {
-    const { isLoading, categories, endCursor, hasNextPage } = this.state;
+  const onLoadMore = async () => {
+    const { isLoading, categories, endCursor, hasNextPage } = state;
     if (!isLoading && hasNextPage) {
-      this.setState({
+      setState({
+        ...state,
         isLoading: true,
       });
+
       const { data } = await apolloClient.query({
         query: VIDEOS_CATEGORIES,
         variables: {
@@ -139,10 +186,11 @@ class VideosArchive extends Component {
 
       const formattedCategories = addCategoryVideosDurations(
         data.categories.nodes,
-        this.props.videoDurations
+        content.videoDurations
       );
 
-      this.setState({
+      setState({
+        ...state,
         categories: [...categories, ...formattedCategories],
         endCursor: data.categories.pageInfo
           ? data.categories.pageInfo.endCursor
@@ -155,44 +203,59 @@ class VideosArchive extends Component {
     }
   };
 
-  // handleLoadMore = () => {
-  //   this.onLoadMore();
-  // };
-
-  onVideoSelect = (url, imageUrl, title, duration, index) => {
-    this.setState({
+  const onVideoSelect = (url, imageUrl, title, duration, index) => {
+    setState({
+      ...state,
       selectedVideo: {
         url: formatYouTubeUrl(url),
         imageUrl,
         title,
         duration,
       },
-    });
-    this.setState({
       selectedIndex: index,
       isPlaying: true,
     });
   };
 
-  onClick = () => {
-    this.setState({ isPlaying: !this.state.isPlaying });
+  const onClick = () => {
+    setState({ ...state, isPlaying: !state.isPlaying });
   };
 
-  render() {
-    const { isLoading } = this.state;
-    const { videos } = this.props;
-    const { isPlaying } = this.state;
-    const { url, imageUrl, title, duration } = this.state.selectedVideo;
+  const { videos } = content;
+
+  if (!videos) {
     return (
       <div className="videos-page">
-        <Head>
-          {/* TODO: change title */}
-          <title>{'Відео'}</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
         <main className="videos-main">
           <div className="container">
+            <div className="row">
+              <VideosPageLoader />
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
+
+  const {
+    isLoading,
+    isPlaying,
+    selectedVideo,
+    categories,
+    selectedIndex,
+  } = state;
+
+  return (
+    <div className="videos-page">
+      <Head>
+        {/* TODO: change title */}
+        <title>{'Відео'}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="videos-main">
+        <div className="container">
+          {selectedVideo && (
             <div className="row">
               <div className="col-lg-8">
                 <div className="pos-relative video-detail__wrapper">
@@ -204,16 +267,18 @@ class VideosArchive extends Component {
                   <iframe
                     allowFullScreen
                     className="w-100 h-100 video-detail__iframe"
-                    src={`${url}${isPlaying ? '?autoplay=1' : ''}`}
+                    src={`${selectedVideo.url}${
+                      isPlaying ? '?autoplay=1' : ''
+                    }`}
                     frameBorder="0"
                     title="video player"
                   />
                   {!isPlaying && (
                     <div
-                      onClick={this.onClick}
+                      onClick={onClick}
                       className="video-detail__cover bg-cover"
                       style={{
-                        backgroundImage: `url(${imageUrl})`,
+                        backgroundImage: `url(${selectedVideo.imageUrl})`,
                       }}
                     >
                       <Play />
@@ -222,35 +287,36 @@ class VideosArchive extends Component {
                 </div>
                 <div className="d-block d-lg-none">
                   <div className="video-detail__duration tx-tiny font-weight-medium tx-family-titles">
-                    {duration}
+                    {selectedVideo.duration}
                   </div>
-                  <h1 className="video-detail__title">{title}</h1>
+                  <h1 className="video-detail__title">{selectedVideo.title}</h1>
                 </div>
               </div>
               <div className="col-lg-4">
                 <VideosList
                   videos={videos}
-                  onVideoSelect={this.onVideoSelect}
-                  selectedIndex={this.state.selectedIndex}
+                  onVideoSelect={onVideoSelect}
+                  selectedIndex={selectedIndex}
                 />
               </div>
               <div className="col-12 d-none d-lg-block">
                 <div className="video-detail__duration tx-tiny font-weight-medium tx-family-titles">
-                  {duration}
+                  {selectedVideo.duration}
                 </div>
-                <h1 className="video-detail__title">{title}</h1>
+                <h1 className="video-detail__title">{selectedVideo.title}</h1>
               </div>
             </div>
-          </div>
-          <div ref={this.categoriesRef}>
-            <VideoCategories categories={this.state.categories} />
-            {isLoading && <VideoLoader />}
-          </div>
-        </main>
-      </div>
-    );
-  }
-}
+          )}
+        </div>
+        <div ref={categoriesRef}>
+          {categories && <VideoCategories categories={categories} />}
+          {isLoading && <VideoLoader />}
+        </div>
+        <Waypoint onEnter={onLoadMore} />
+      </main>
+    </div>
+  );
+};
 
 VideosArchive.propTypes = {
   videos: PropTypes.arrayOf(
@@ -266,7 +332,11 @@ VideosArchive.propTypes = {
   categories: PropTypes.array,
 };
 
-VideosArchive.getInitialProps = async () => {
+VideosArchive.getInitialProps = async ({ query }) => {
+  if (process.browser) {
+    return { query };
+  }
+
   const { data } = await apolloClient.query({
     query: VIDEOS_ARCHIVE,
   });
