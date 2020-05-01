f3416afb22de4a6ad87d76b360c4de939b3d1b5c
Vlad Romanenko <vlad@outright.digital>
2020-05-01 12:34:29 +0300

    Add publication page loader

--- src/components/Loaders/PublicationMainLoader.js
+++ src/components/Loaders/PublicationMainLoader.js
@@ -5,14 +5,15 @@ const PublicationMainLoader = () => {
   return (
     <div className={'container'}>
       <ContentLoader
-        viewBox={'0 0 1320 705'}
+        viewBox={'0 0 1320 1205'}
         style={{ backgroundColor: 'rgba(0,0,0,0.01)' }}
       >
-        <rect x={'200'} y={'455'} width={'921'} height={`15`} />
-        <rect x={'200'} y={'475'} width={'921'} height={`20`} />
-        <rect x={'200'} y={'500'} width={'921'} height={`20`} />
-        <rect x={'200'} y={'525'} width={'921'} height={`10`} />
-        <rect x={'200'} y={'540'} width={'921'} height={`10`} />
+        <rect x={'0'} y={'50'} width={'1320'} height={`700`} />
+
+        <rect x={'0'} y={'790'} width={'1320'} height={`20`} />
+
+        <rect x={'0'} y={'820'} width={'650'} height={`600`} />
+        <rect x={'670'} y={'820'} width={'650'} height={`600`} />
       </ContentLoader>
     </div>
   );
