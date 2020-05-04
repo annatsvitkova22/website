1ca364c0afd7b350143f88a379e80b7bbaa2e15c
Vlad Romanenko <vlad@outright.digital>
2020-05-04 14:52:09 +0300

    Add blogger loader

--- src/components/Loaders/Blogger.js
+++ src/components/Loaders/Blogger.js
@@ -3,42 +3,33 @@ import ContentLoader from 'react-content-loader';
 
 const BloggerLoader = () => {
   return (
-    <ContentLoader viewBox={'0 0 1210 900'}>
-      <rect x={'0'} y={'0'} rx={'50'} ry={'50'} width={'95'} height={`95`} />
-      <rect x={'0'} y={'100'} width={'170'} height={`15`} />
-      <rect x={'0'} y={'120'} width={'170'} height={`15`} />
-      <rect x={'0'} y={'140'} width={'170'} height={`15`} />
-      <rect x={'0'} y={'160'} width={'170'} height={`15`} />
+    <ContentLoader viewBox={'0 0 1312 1000'}>
+      <rect x={'0'} y={'0'} rx={'100'} ry={'100'} width={'200'} height={`200`} />
+      <rect x={'0'} y={'226'} width={'150'} height={`16`} />
+      <rect x={'0'} y={'246'} width={'130'} height={`16`} />
+      <rect x={'0'} y={'286'} width={'200'} height={`20`} />
+      <rect x={'0'} y={'320'} width={'80'} height={`20`} />
+      <rect x={'0'} y={'360'} width={'406'} height={`100`} />
 
-      <rect x={'300'} y={'0'} width={'450'} height={`190`} />
-      <rect x={'300'} y={'195'} width={'450'} height={`15`} />
-      <rect x={'300'} y={'215'} width={'450'} height={`25`} />
-      <rect x={'300'} y={'245'} width={'450'} height={`15`} />
+      <rect x={'448'} y={'0'} width={'424'} height={`250`} />
+      <rect x={'448'} y={'267'} width={'86'} height={`21`} />
+      <rect x={'448'} y={'305'} width={'420'} height={`90`} />
+      <rect x={'448'} y={'412'} width={'138'} height={`16`} />
 
-      <rect x={'765'} y={'0'} width={'450'} height={`190`} />
-      <rect x={'765'} y={'195'} width={'450'} height={`15`} />
-      <rect x={'765'} y={'215'} width={'450'} height={`25`} />
-      <rect x={'765'} y={'245'} width={'450'} height={`15`} />
+      <rect x={'892'} y={'0'} width={'424'} height={`250`} />
+      <rect x={'892'} y={'267'} width={'86'} height={`21`} />
+      <rect x={'892'} y={'305'} width={'420'} height={`90`} />
+      <rect x={'892'} y={'412'} width={'138'} height={`16`} />
 
-      <rect x={'300'} y={'300'} width={'450'} height={`190`} />
-      <rect x={'300'} y={'495'} width={'450'} height={`15`} />
-      <rect x={'300'} y={'515'} width={'450'} height={`25`} />
-      <rect x={'300'} y={'545'} width={'450'} height={`15`} />
+      <rect x={'448'} y={'548'} width={'424'} height={`250`} />
+      <rect x={'448'} y={'815'} width={'86'} height={`21`} />
+      <rect x={'448'} y={'853'} width={'420'} height={`90`} />
+      <rect x={'448'} y={'960'} width={'138'} height={`16`} />
 
-      <rect x={'765'} y={'300'} width={'450'} height={`190`} />
-      <rect x={'765'} y={'495'} width={'450'} height={`15`} />
-      <rect x={'765'} y={'515'} width={'450'} height={`25`} />
-      <rect x={'765'} y={'545'} width={'450'} height={`15`} />
-
-      <rect x={'300'} y={'600'} width={'450'} height={`190`} />
-      <rect x={'300'} y={'795'} width={'450'} height={`15`} />
-      <rect x={'300'} y={'815'} width={'450'} height={`25`} />
-      <rect x={'300'} y={'845'} width={'450'} height={`15`} />
-
-      <rect x={'765'} y={'600'} width={'450'} height={`190`} />
-      <rect x={'765'} y={'795'} width={'450'} height={`15`} />
-      <rect x={'765'} y={'815'} width={'450'} height={`25`} />
-      <rect x={'765'} y={'845'} width={'450'} height={`15`} />
+      <rect x={'892'} y={'548'} width={'424'} height={`250`} />
+      <rect x={'892'} y={'815'} width={'86'} height={`21`} />
+      <rect x={'892'} y={'853'} width={'420'} height={`90`} />
+      <rect x={'892'} y={'960'} width={'138'} height={`16`} />
     </ContentLoader>
   );
 };
