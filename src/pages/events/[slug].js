4c8f6606a44c1fc49782e84d375200cb3abb0ac0
Deekline <46416799+Deekline@users.noreply.github.com>
2020-05-05 13:16:31 +0300

    Add checking existing of event date and implement showing this date on cf and opp

--- src/pages/events/[slug].js
+++ src/pages/events/[slug].js
@@ -143,6 +143,14 @@ const Event = (props) => {
     };
   }, []);
 
+  const [isTimeExist, setIsTimeExist] = useState(false);
+
+  useEffect(() => {
+    if (state.event && state.event.zmAfishaACF.eventDate) {
+      setIsTimeExist(true);
+    }
+  }, [state.event]);
+
   if (!event) {
     return (
       <div className="single__event">
@@ -178,9 +186,12 @@ const Event = (props) => {
               }
             >
               <div className="event__hero-inner container">
-                <EventHeader event={event} />
+                <EventHeader event={event} withTime={isTimeExist} />
                 <div className="event__info-card">
-                  <EventsLikeSidebar data={event.zmAfishaACF} withTime={true} />
+                  <EventsLikeSidebar
+                    data={event.zmAfishaACF}
+                    withTime={isTimeExist}
+                  />
                 </div>
               </div>
             </div>
