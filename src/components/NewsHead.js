88a76c82f052c5a18ad499ca808a3045bbe64d7c
Deekline <46416799+Deekline@users.noreply.github.com>
2020-05-06 13:00:06 +0300

    Add expand button and popup for main image on news

--- src/components/NewsHead.js
+++ src/components/NewsHead.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import Tags from '~/components/Tags';
 import PostHeaderLoader from '~/components/Loaders/PostHeaderLoader';
 import ArticleTaxonomies from '~/components/Article/Taxonomies';
 
