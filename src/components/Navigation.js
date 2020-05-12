eb447010778b04d0e2f1c24c535c4078ed467778
Deekline <46416799+Deekline@users.noreply.github.com>
2020-05-12 11:13:31 +0300

    Add hover to social icons on header mobile version, delete isSelected option for header navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,40 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import * as classnames from 'classnames';
-import { useRouter } from 'next/router';
 
 import NavLink from './SiteLink';
 
-const Navigation = (props) => {
-  const { navigationData, className = '' } = props;
-  const [route, setRoute] = useState(null);
-
-  const router = useRouter();
-
-  useEffect(() => {
-    const routes = router.asPath.substring(router.asPath.indexOf('/'), 5);
-
-    setRoute(routes);
-  }, [router]);
-
-  const naviData = navigationData.nodes.filter((item) => {
-    return item.name === 'Головне';
-  });
-
+const Navigation = ({ navigationData, className = '' }) => {
   return (
     <nav className={`${className}`}>
       <ul className="navigation__list">
-        {naviData &&
-          naviData[0].menuItems.nodes &&
-          naviData[0].menuItems.nodes.map((item) => {
+        {navigationData &&
+          navigationData.nodes &&
+          navigationData.nodes[0].menuItems.nodes.map((item) => {
             return (
               <li
                 key={item.id}
                 className={classnames('navigation__item', {
                   'navigation__item--highlighted':
                     item.menuItemACF.ishighlighted,
-                  // TODO: restore working version
-                  // 'navigation__item--active': item.url.match(route),
                 })}
               >
                 <NavLink
