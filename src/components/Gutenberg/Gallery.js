4cd77277ba654c176e0cc659071f367abba689d1
Deekline <46416799+Deekline@users.noreply.github.com>
2020-05-18 11:12:39 +0300

    Parse gallery data, add gallery component

--- src/components/Gutenberg/Gallery.js
+++ src/components/Gutenberg/Gallery.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import * as _ from 'lodash';
+import ImageGallery from 'react-image-gallery';
 
 const Gallery = ({ block }) => {
-  // const array = block.attributes.images;
-  // const reg = /[\[\]\{\}\\]/gm;
-  // const regTwo = /\,/gm;
-  // const anotherreg = /(^.*:")/gm;
-  // const newArr = array.replace(reg, '').replace(regTwo, ' ').split(' ');
-  // const chunk = newArr.length / 7;
-  // const splitArr = (arr, chunks) =>
-  //   arr.reduce(
-  //     // eslint-disable-next-line no-return-assign
-  //     (acc, n, i) => ((acc[i % chunks] = acc[i % chunks] || []).push(n), acc),
-  //     []
-  //   );
-  // const arr = splitArr(newArr, chunk);
-  // console.log(arr);
-  // const newData = arr.map((item) => {
-  //   return item.map((element) =>
-  //     element.replace(anotherreg, '').replace('"', '')
-  //   );
-  // });
+  const [pictures, setPictures] = useState([]);
+  console.log(block);
+  const array = block.attributes.images;
+  const reg = /[\[\]\{\}\\]/gm;
+  const regTwo = /\,/gm;
+  const newArr = array.replace(reg, '').replace(regTwo, ' ').split(' ');
 
-  return <div dangerouslySetInnerHTML={{ __html: block.saveContent }} />;
+  const dateToArray = _.chunk(newArr, 6).map((item) => {
+    return new Map(
+      item.map((el) => {
+        return el
+          .replace(/\"/, '')
+          .slice(0, -1)
+          .split(/\"\:\"(.+)/);
+      })
+    );
+  });
+
+  const dateToObject = dateToArray.map((item) => {
+    return Object.fromEntries(item);
+  });
+
+  useEffect(() => {
+    const temp = [];
+    dateToObject.forEach((item) => {
+      temp.push({
+        original: item.url,
+        thumbnail: item.url,
+        description: item.caption,
+        originalAlt: item.id,
+      });
+    });
+    setPictures(temp);
+  }, []);
+  console.log(dateToObject);
+
+  console.log(pictures);
+  return (
+    <div>
+      <ImageGallery items={pictures} />
+    </div>
+  );
 };
 Gallery.propTypes = {
   block: PropTypes.any,
